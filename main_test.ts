f767eb5c7ee1f77debfcaf0873fe74fd2f7542af
Axhon <axhonruiz@gmail.com>
2023-10-04 00:13:21 -0400

    use import map

--- main_test.ts
+++ main_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
+import { assertEquals } from "std/assert/mod.ts";
 import { add } from "./main.ts";
 
 Deno.test(function addTest() {
