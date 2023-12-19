e943382cd14cb7a2e0c442e34e258f69308a6509
Axhon <axhonruiz@gmail.com>
2023-12-18 23:13:13 -0500

    update deps and keep working on test

--- lexer/lexer.test.ts
+++ lexer/lexer.test.ts
@@ -2,7 +2,7 @@ import { assert } from "std/assert/mod.ts";
 import { Token, TokenType } from "../token.ts";
 import { Lexer } from "./lexer.ts";
 
-Deno.test("lexer.nextToken()", (t) => {
+Deno.test("lexer.nextToken()", async (t) => {
   const input = "=+(){},;";
   const tests: { expectedType: TokenType; expectedLiteral: string }[] = [
     { expectedType: "ASSIGN", expectedLiteral: "=" },
@@ -21,7 +21,7 @@ Deno.test("lexer.nextToken()", (t) => {
   for (const test of tests) {
     const token = lexer.nextToken();
 
-    t.step(test.expectedLiteral, () => {
+    await t.step(test.expectedLiteral, () => {
       assert(test.expectedType === token.type);
       assert(test.expectedLiteral === token.literal);
     });
